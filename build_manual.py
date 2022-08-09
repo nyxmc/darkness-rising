cae1f565d72aeb59d0647832fc2382eb2b8eb376
Oskar Manhart <52569953+PixelAgent007@users.noreply.github.com>
2022-08-09 10:37:21 +0200

    Finished manual and mmc build scripts

--- build_manual.py
+++ build_manual.py
@@ -3,12 +3,9 @@ from zipfile import ZipFile
 
 os.mkdir("build")
 
-with ZipFile("build/Darkness_Rising_MMC.zip", mode="w") as archive:
-    archive.write("mmc-pack.json")
-    archive.write("instance.cfg")
-
+with ZipFile("build/Darkness_Rising_Manual.zip", mode="w") as archive:  
     # crawling through directory and subdirectories
-    for root, directories, files in os.walk(".minecraft"):
+    for root, directories, files in os.walk(".minecraft/mods"):
         for filename in files:
             # join the two strings in order to form the full filepath.
             filepath = os.path.join(root, filename)
